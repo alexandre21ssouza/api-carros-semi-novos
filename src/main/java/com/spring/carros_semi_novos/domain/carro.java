0aa1b447904e28d000ddc4ef0193ef23e99e7fd3
alexandre21ssouza <alexandre2121ssouza@gmail.com>
2025-02-10 21:34:54 -0300

    Atributos da entidade Carro

--- src/main/java/com/spring/carros_semi_novos/domain/carro.java
+++ src/main/java/com/spring/carros_semi_novos/domain/carro.java
@@ -1,4 +1,35 @@
 package com.spring.carros_semi_novos.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "carros")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class carro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String modelo;
+
+    @Column(nullable = false)
+    private String marca;
+
+    @Column(nullable = false)
+    private String placa;
+
+    @Column(nullable = false)
+    private int ano;
+
+    @Column(nullable = false)
+    private int preço;
 }
