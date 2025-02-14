96736c8cf5a2545082f37230a9cb2f987ae953dc
alexandre21ssouza <alexandre2121ssouza@gmail.com>
2025-02-13 23:05:24 -0300

    Acionando a exceception no service

--- src/main/java/com/spring/carros_semi_novos/services/VeiculoService.java
+++ src/main/java/com/spring/carros_semi_novos/services/VeiculoService.java
@@ -1,6 +1,7 @@
 package com.spring.carros_semi_novos.services;
 
 import com.spring.carros_semi_novos.domain.Veiculo;
+import com.spring.carros_semi_novos.exception.VeiculoNotFoundException;
 import com.spring.carros_semi_novos.repository.VeiculoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,8 +19,13 @@ public class VeiculoService {
         return  repository.findAll();
     }
 
-    public Optional<Veiculo> buscarPorId(Long id){
-        return repository.findById(id);
+    public Veiculo buscarPorId(Long id){
+
+        Veiculo veiculo = repository.findById(id).orElse(null);
+        if (veiculo == null){
+            throw new VeiculoNotFoundException("Veículo não encontrado");
+        }
+        return veiculo;
     }
 
     public Veiculo salvar(Veiculo veiculo){
