a9d5812c92b973fc1e26715c6242d83660bd1808
alexandre21ssouza <alexandre2121ssouza@gmail.com>
2025-02-13 22:22:40 -0300

    Construção do pacote e Classe DTO

---
+++ src/main/java/com/spring/carros_semi_novos/dto/CarroDTO.java
@@ -0,0 +1,18 @@
+package com.spring.carros_semi_novos.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CarroDTO {
+    private String modelo;
+    private String marca;
+    private String placa;
+    private int ano;
+    private double preco;
+}
